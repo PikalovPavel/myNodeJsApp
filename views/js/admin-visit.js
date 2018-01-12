0a5a625ec1c69dc0f5c6d32c7d564d385647bf14
PikalovPavel <iilaughyoudie@gmail.com>
2018-01-12 21:23:06 +0300

    little fix

---
+++ views/js/admin-visit.js
@@ -0,0 +1,26 @@
+var visits=null;
+function loadVisitors() {
+    var register = {};
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', '/api/allVisits', false);
+    xhr.send();
+    if (xhr.status != 200) {
+        Console.log('Ошибка ' + xhr.status + ': ' + xhr.statusText);
+    } else {
+        visitors = JSON.parse(xhr.responseText);
+        for (k in visitors) {
+            var arr;
+            arr=visitors[k]['ДАТА_ПОСЕЩЕНИЯ'].split("T");
+            visitors[k]['ДАТА_ПОСЕЩЕНИЯ']=arr[0];
+        }
+    }
+    console.log(visitors);
+    visits=visitors ;
+}
+loadVisitors();
+var app2 = new Vue ({
+    el: '#appe',
+    data: {
+        messagee:visits
+    }
+});
